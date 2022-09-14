587ac300ebb23d263e562717c910a3af9f4bda7a
phillip khoza <phillipantonk@gmail.com>
2022-09-14 13:29:00 +0200

    project init

---
+++ firebase.config.js
@@ -0,0 +1,10 @@
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCqpH7_ixCaQZM3P2fnHSlUeKUCXgNkhUk",
+    authDomain: "express-firebase-fd39c.firebaseapp.com",
+    projectId: "express-firebase-fd39c",
+    storageBucket: "express-firebase-fd39c.appspot.com",
+    messagingSenderId: "711553963398",
+    appId: "1:711553963398:web:f9a1ac33cebd1544754807"
+  };
